bfb11d724ead9823c0eec2939c40f15de4dcb8dc
srenshaw <srenshaw1@gmail.com>
2024-09-27 02:30:16 -0500

    - Simplified main.rs to a basic CLI interface. - Added tracing library for logging.

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,30 @@
 
+use miette::IntoDiagnostic;
+use tracing::{info,debug};
+
 mod tokens;
 mod lexer;
 mod parser;
 
 fn main() -> miette::Result<()> {
-	let mut buffer = String::default();
+	tracing_subscriber::fmt::init();
 
-	loop {
-		buffer.clear();
+	let mut args = std::env::args();
+	args.next();
 
-		if let Err(e) = std::io::stdin().read_line(&mut buffer) {
-			eprintln!("{e}");
-			continue;
-		}
+	let file_name = args.next()
+		.expect("missing source file");
 
-		if buffer.trim() == ":exit" {
-			break;
-		}
+	let file = std::fs::read_to_string(&file_name.trim())
+		.into_diagnostic()?;
 
-		let tokens = lexer::eval(&buffer)?;
-		let ast = parser::eval(&buffer, tokens)?;
-		println!("{ast:#?}");
-		//stage1::eval(&ast, 0);
-	}
+	info!("lexing");
+	let tokens = lexer::eval(&file)?;
+	debug!("{}", tokens.iter().map(|t| format!("{t}")).collect::<Vec<_>>().join(" "));
+	info!("parsing");
+	let ast = parser::eval(&file, tokens)?;
+	debug!("{ast:#?}");
 
 	Ok(())
 }
 
-/*
-mod stage1 {
-	use crate::tokens::TokenType;
-	use crate::parser::S;
-
-	const R_MAX: usize = 1;
-
-	pub(crate) fn eval(ast: &S, r: usize) {
-		match ast {
-			S::Expr(token) => match token.tt {
-				TokenType::Num => {
-					let num = token.to_string().parse::<f64>().unwrap();
-					if num.fract().abs() < 0.00000001 && (i8::MIN..=i8::MAX).contains(&(num.trunc() as i8)) {
-						if r < R_MAX {
-							println!("MOV #{},R{r}", num.trunc() as i8);
-						} else {
-							println!("MOV #{},@-SP", num.trunc() as i8);
-						}
-					} else if num.fract().abs() < 0.00000001 && (i16::MIN..=i16::MAX).contains(&(num.trunc() as i16)) {
-						todo!("store i16 value and indirectly load into R0");
-					} else if num.fract().abs() < 0.00000001 && (i32::MIN..=i32::MAX).contains(&(num.trunc() as i32)) {
-						todo!("store i32 value and indirectly load into R0")
-					}
-				}
-				_ => eprintln!("ERROR: non-numeric literal return value from program"),
-			}
-			S::List(list) => match &list[..] {
-				[] => {}
-				[first, rest @ ..] => {
-					match first {
-						S::Expr(op) => match op.tt {
-							TokenType::Plus => {
-								if r < R_MAX {
-									println!("MOV #0,R{r}");
-								} else {
-									println!("MOV #0,@-SP");
-								}
-								for item in rest {
-									match item {
-										S::Expr(t) => if r < R_MAX {
-											println!("ADD #{},R{r}", t.to_string());
-										} else {
-											println!("MOV @SP+,R{R_MAX}");
-											println!("ADD #{},R{R_MAX}", t.to_string());
-											println!("MOV R{R_MAX},@-SP");
-										}
-										_ => {
-											eval(item, r+1);
-											if r+1 < R_MAX {
-												println!("ADD R{},R{r}", r+1);
-											} else {
-												println!("MOV @SP+,R{R_MAX}");
-												println!("ADD R{R_MAX},R{r}");
-											}
-										}
-									}
-								}
-							}
-							TokenType::Minus => {}
-							TokenType::Star => {}
-							TokenType::Slash => {}
-							//TokenType::SlashPercent => {}
-							TokenType::Percent => {}
-							//TokenType::DbLArrow => {}
-							//TokenType::DbRArrow => {}
-							//TokenType::Diamond => {}
-							TokenType::LArrow1 => {}
-							//TokenType::LArrEq => {}
-							TokenType::RArrow1 => {}
-							//TokenType::RArrEq => {}
-							_ => eprintln!("Unknown Op '{first}'"),
-						}
-						S::List(_) => {}
-					}
-				}
-			}
-		}
-	}
-}
-*/

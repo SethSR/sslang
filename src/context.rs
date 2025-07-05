2ffecc7783e15bfafd3e618baa7bed239fadd10a
SethSR <SethSR@users.noreply.github.com>
2025-07-05 04:19:24 -0500

    Added the context guard for RAII style use.

--- src/context.rs
+++ src/context.rs
@@ -1,10 +1,21 @@
 
-pub(crate) struct Context {
+pub(crate) struct Data {
 	frames: Vec<String>,
 }
 
-impl Context {
-	pub fn push(&mut self, s: &str) {
-		self.frames.push(s);
+pub(crate) struct Guard<'a> {
+	ctx: &'a mut Data,
+}
+
+impl<'a> Guard<'a> {
+	pub fn new(ctx: &'a mut Data, s: impl Into<String>) -> Self {
+		ctx.frames.push(s.into());
+		Self { ctx }
+	}
+}
+
+impl Drop for Guard<'_> {
+	fn drop(&mut self) {
+		self.ctx.frames.pop();
 	}
 }
